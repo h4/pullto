41149c3ecb5fe2076f7c8cce029ed81d1802be70
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-04-20 17:51:34 +0400

    Добавил файл настроек

--- settings.sample.py
+++ settings.sample.py
@@ -2,9 +2,15 @@
 
 DEBUG = True
 
+TRAVIS_TOKEN = 'yourTravisToken'
+
+GITHUB_USER = 'h4'
+
+GITHUB_REPO = 'pullto'
+
 REPOSITORY_URL = "https://github.com/h4/pullto"
 
-BRANCHES_DIRS = {
+BRANCH_DIRS = {
     'master': 'PATH_TO_DIR_FOR_MASTER',
     'develop': 'PATH_TO_DIR_FOR_DEVELOP'
 }
