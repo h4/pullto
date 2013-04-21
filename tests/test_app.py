4bfa221525fe1c89f924aecf41d0230b6c6776b8
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-04-21 16:32:23 +0400

    Переписал тесты приложения в виде метода тесткейса

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,32 +8,29 @@ import app as application
 current_dir = os.path.dirname(__file__)
 
 
-def test_index_page():
-    app = TestApp(application.app)
-
-    assert app.get('/', expect_errors=True).status_int == 400
-    assert app.post('/', expect_errors=True).status_int == 401
-
-    token = 'wrong_token'
-
-    assert app.post('/', expect_errors=True, headers={'Authorization': token}).status_int == 401
-
-
 class Test(unittest.TestCase):
+    def setUp(self):
+        payload_file = os.path.join(current_dir, 'travis-payload.json')
+
+        self.valid_token = '35ca10f4f974ef107cde3243dca2bdd56df683c2b8cdfa45b3c4672c759e9f5a'
+        self.invalid_token = 'Broken_Token'
+        self.payload = json.load(open(payload_file))
+
     def test_check_auth(self):
-
-        valid_token = '35ca10f4f974ef107cde3243dca2bdd56df683c2b8cdfa45b3c4672c759e9f5a'
-        invalid_token = 'Broken_Token'
-
-        expected_result = application.check_auth(valid_token)
-        unexpected_result = application.check_auth(invalid_token)
+        expected_result = application.check_auth(self.valid_token)
+        unexpected_result = application.check_auth(self.invalid_token)
 
         self.assertTrue(expected_result)
         self.assertFalse(unexpected_result)
 
-    def setUp(self):
-        payload_file = os.path.join(current_dir, 'travis-payload.json')
-        self.payload = json.load(open(payload_file))
-
     def test_verify_status(self):
         self.assertTrue(application.verify_status(self.payload))
+
+    def test_index_page(self):
+        app = TestApp(application.app)
+
+        assert app.get('/', expect_errors=True).status_int == 400
+        assert app.post('/', expect_errors=True).status_int == 401
+
+        assert app.post('/', expect_errors=True, headers={'Authorization': self.invalid_token}).status_int == 401
+        assert app.post('/', expect_errors=True, headers={'Authorization': self.valid_token}, ).status_int == 500
